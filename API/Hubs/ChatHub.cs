c5b8c5281b01816128fb6463eccc21c6f2ff256c
ttphu2 <ttphu34@gmail.com>
2021-07-10 11:30:32 +0700

    Init project

---
+++ API/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace API.Hubs
+
+{
+    public class ChatHub : Hub
+    {
+        public Task SendMessage1(string user, string message)               // Two parameters accepted
+        {
+            return Clients.All.SendAsync("ReceiveOne", user, message);    // Note this 'ReceiveOne' 
+        }
+    }
+}
